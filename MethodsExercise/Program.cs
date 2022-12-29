d484f18d2014ce11fb2522f3656e05b3f9d37276
ross dibartolo <dibartoloross@gmail.com>
2022-12-29 12:57:31 -0500

    Exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -9,6 +9,12 @@
             Console.WriteLine(myStory);
 
             // Exercise 2
+            int x = 2;
+            int y = 3;
+            Console.WriteLine(Add(x, y));
+            Console.WriteLine(Sub(x, y));
+            Console.WriteLine(Div(x, y));
+            Console.WriteLine(Mult(x, y));
 
         }
         public static string Story()
@@ -25,9 +31,26 @@
             Console.WriteLine("What is your favorite band?");
             var band = Console.ReadLine();
 
-            var story = $"Hello! My name is {name}. {color} is my favorite color, {animal} is my favorite animal, and {band} is my favorite band.";
+            var story = $"Hello! My name is {name}. {color} is my favorite color, {animal} are my favorite animal, and {band} is my favorite band.";
             return story;
            
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+        public static int Mult(int x, int y)
+        {
+            return x * y;
         }
 
 
