d3dc44deb3de917c67059cf3e1718eb2dcc81bdf
ross dibartolo <dibartoloross@gmail.com>
2022-12-29 12:37:35 -0500

    Exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Exercise 1
+            var myStory = Story();
+            Console.WriteLine(myStory);
+
+            // Exercise 2
+
         }
+        public static string Story()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            var story = $"Hello! My name is {name}. {color} is my favorite color, {animal} is my favorite animal, and {band} is my favorite band.";
+            return story;
+           
+        }
+
+
     }
+
+ 
 }
